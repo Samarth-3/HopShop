d497ec621602bc3bfa2076380a9e1f835ab2d1b4
Samarth-3 <spaliwal_be21@thapar.edu>
2023-07-18 16:24:13 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-
+import React from "react";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-const App = () => {
-  return (
+import ProductPage from "./pages/ProductPage";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
 
-    <div>
-      <ProductList/>
-    </div>
+import { BrowserRouter as Router, Route, Routes,Navigate } from "react-router-dom";
+const App = () => {
+  const user= true;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/products/:category" element={<ProductList />} />
+        <Route exact path="/product/:id" element={<ProductPage />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route exact path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+
+      </Routes>
+    </Router>
   );
 };
 
