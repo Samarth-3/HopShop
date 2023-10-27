498405f0a60a0a42d8ddc43bca7c281715ece815
Samarth-3 <spaliwal_be21@thapar.edu>
2023-10-27 15:15:20 +0530

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,8 +4,6 @@ import "./home.css";
 import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
-
-
 export default function Home() {
   return (
     <div className="home">
